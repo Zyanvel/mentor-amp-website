3070f4b6a256f983393319584308bbe6d30fb1e9
Zyanvel <zyanvel@gmail.com>
2021-01-30 11:45:22 -0800

    Updated the first component but I will need to add it to the app.js after pushing my changes

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        <img src='/TempLogo.png' alt='Mentor Amp Logo'/>
+                    </Link>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
