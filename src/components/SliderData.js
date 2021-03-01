74798941fe532c3b71e819f0ae3dc00b0f8da5fd
Albert Koshy <Albertkoshy@gmail.com>
2021-02-28 17:45:11 -0800

    carousel

---
+++ src/components/SliderData.js
@@ -0,0 +1,15 @@
+import img1 from '../image/Arturo.png'
+import img2 from '../image/Brianna.png'
+import img3 from '../image/Preethi.png'
+
+export const SliderData = [
+    {
+        image: img1
+    },
+    {
+        image: img2
+    },
+    {
+        image: img3
+    }
+];
