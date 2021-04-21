c601e4c31d0bfcdfaa23257ea5112c94c473220e
spagnon7 <spagnon7@gmail.com>
2021-04-20 20:56:24 -0700

    preserved image ratios in carousel and made components responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
-    return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        <img src='/LOGO_official.png' alt='Mentor Amp Logo'/>
-                    </Link>
-                </div>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            <img src="/LOGO_official.png" alt="Mentor Amp Logo" />
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default NavBar
+export default NavBar;
