d2f5065e7c2070650d2f7651c54895125043767f
eye-opener-tenzin <tmejay.tm@gmail.com>
2021-05-11 21:07:23 -0700

    comments on navebar components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,8 @@ function NavBar() {
     <>
       <nav className="navbar">
         <div className="navbar-container">
+          /* link logo to the homepage  &
+           import logo from the public folder*/
           <Link to="/" className="navbar-logo">
             <img src="/LOGO_official.png" alt="Mentor Amp Logo" />
           </Link>
