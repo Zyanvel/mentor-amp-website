97d128f7fdf0f167250d6e41dcefe12a2b0a25de
Albert Koshy <Albertkoshy@gmail.com>
2021-05-20 21:17:00 -0700

    made the component turn into a column when downsizing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,9 +6,8 @@ function NavBar() {
   return (
     <>
       <nav className="navbar">
-        <div className="navbar-container">
-          /* link logo to the homepage  &
-           import logo from the public folder*/
+        <div className="navbar-container" /* link logo to the homepage  &
+           import logo from the public folder*/ >
           <Link to="/" className="navbar-logo">
             <img src="/LOGO_official.png" alt="Mentor Amp Logo" />
           </Link>
